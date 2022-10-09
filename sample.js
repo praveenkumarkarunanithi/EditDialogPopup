e4ecc836216267cbc97cc3e6c04303653d1e773f
praveenkumarkarunanithi <100338903+praveenkumarkarunanithi@users.noreply.github.com>
2022-10-09 19:04:52 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,7 +1,11 @@
 
 var grid = new ej.grids.Grid({
     dataSource: data,
+    enableAdaptiveUI: true,
     editSettings: { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Normal' },
+    load: () => {
+      grid.adaptiveDlgTarget = document.getElementsByClassName('e-mobile-content')[0];
+  },
     columns: [
       {field: 'OrderID',headerText: 'Order ID',width: 120,isPrimaryKey: true},
       {field: 'ShipCity',headerText: 'Ship City',width: 130},
@@ -10,5 +14,5 @@ var grid = new ej.grids.Grid({
     ],
   });
 
-grid.appendTo('#Grid');
+grid.appendTo('#adaptivebrowser');
 
